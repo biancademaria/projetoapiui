1958a6c102de1bc04bcf3df18df87e47d1694a22
RogerioFurlan <linqsys@uol.com.br>
2020-04-02 12:30:48 -0300

    Projeto Angular Signed-off-by: RogerioFurlan <rfurlanbb@gmail.com>

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { CidadesModule } from './cidades/cidades.module';
+import { RouterModule, Routes } from '@angular/router';
+import { CidadesPesquisaComponent } from './cidades/cidades-pesquisa/cidades-pesquisa.component';
+import { CidadesService } from './cidades/cidades.service';
+import { HttpClientModule } from '@angular/common/http';
+
+const routes: Routes = [
+  {path: 'cidades', component: CidadesPesquisaComponent}
+]
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    AppRoutingModule,
+    CoreModule, 
+    CidadesModule
+   
+  ],
+  providers: [CidadesService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
